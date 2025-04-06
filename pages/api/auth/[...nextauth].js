9c28ab2c1fa7c87526affb77110a609806ffcc69
Aman14-ai <amancode2005@gmail.com>
2025-04-06 12:24:59 +0530

    changes in auth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,11 +5,11 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
     providers: [
         GithubProvider({
-            clientId:'Ov23lijPtiyILPNIK3Bs',
-            clientSecret: 'd1399f80f1c8aed00dab9b03106dce0dbfe2974c'
-        }),
-       
-    ]
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+          }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions);
